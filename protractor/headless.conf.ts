5341e8eb0980ebbc7871a29c39848f2aefa7a8bf
alexanderjaramillo <john.jara@gmail.com>
2017-10-10 10:30:19 -0500

    loading-time-jasmine

--- protractor/headless.conf.ts
+++ protractor/headless.conf.ts
@@ -6,7 +6,7 @@ export const config: Config = {
   SELENIUM_PROMISE_MANAGER: false,
   specs: ['../test/**/*.spec.js'],
   noGlobals: true,
-  getPageTimeout: 1000,
+  getPageTimeout: 30000,
   onPrepare: () => {
     browser.ignoreSynchronization = true ;
     reporter();
