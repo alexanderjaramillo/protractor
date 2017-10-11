ba626aa705d57ddb22accaf1574078b947168bfe
alexanderjaramillo <john.jara@gmail.com>
2017-10-11 07:59:41 -0500

    improving-locators fixes

--- src/page/AddressStep.page.ts
+++ src/page/AddressStep.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class AddressStepPage {
   private get proceedToCheckoutButton(): ElementFinder {
-    return $('[name=\'processAddress\']');
+    return $('[name="processAddress"]');
   }
   
   public goToProceedCheckout(): promise.Promise<void> {
