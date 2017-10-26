3abcfcf42d94963d19e60beab34e293bc3cc3b78
alexanderjaramillo <john.jara@gmail.com>
2017-10-26 17:20:15 -0500

    js-code-execution fix

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -1,8 +1,8 @@
-import { browser, $, promise } from 'protractor';
+import { browser, $ } from 'protractor';
 
 export class IFramePage {
-  public modifyIframeHeight(height : number) : promise.Promise<void> {
-    return browser.executeScript(
+  public async modifyIframeHeight(height : number) : Promise<void> {
+    await browser.executeScript(
       `document.querySelector("#IF1").setAttribute('height', ${height});`);
   }
 
