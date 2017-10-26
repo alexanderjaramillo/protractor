d4c525b393d7ade4c4b7305304fe5c0fd4ed1f69
alexanderjaramillo <john.jara@gmail.com>
2017-10-26 17:17:26 -0500

    js-code-execution fix

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -1,13 +1,12 @@
-import { promise, browser } from 'protractor';
+import { browser, $, promise } from 'protractor';
 
 export class IFramePage {
   public modifyIframeHeight(height : number) : promise.Promise<void> {
-    // tslint:disable-next-line:max-line-length
-    return browser.executeScript(`document.querySelector("#IF1").setAttribute('height', ${height});`);
+    return browser.executeScript(
+      `document.querySelector("#IF1").setAttribute('height', ${height});`);
   }
 
   public async getIframeHeight() : Promise<number> {
-    // tslint:disable-next-line:max-line-length
-    return Number(await browser.executeScript(`document.querySelector("#IF1").getAttribute('height');`));
+    return Number(await $('#IF1').getAttribute('height'));
   }
 }
