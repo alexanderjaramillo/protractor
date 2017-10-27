880d7a1029775982c3cd203e94ccb9f3319f9a62
alexanderjaramillo <john.jara@gmail.com>
2017-10-27 08:07:53 -0500

    js-code.execution

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -1,12 +1,16 @@
-import { browser, $ } from 'protractor';
+import { browser, ElementFinder, element, by } from 'protractor';
 
 export class IFramePage {
+  private get IFrame1(): ElementFinder {
+    return element(by.id('IF1'));
+  }
+  
   public async modifyIframeHeight(height : number) : Promise<void> {
     await browser.executeScript(
       `document.querySelector("#IF1").setAttribute('height', ${height});`);
   }
 
   public async getIframeHeight() : Promise<number> {
-    return Number(await $('#IF1').getAttribute('height'));
+    return Number(await this.IFrame1.getAttribute('height'));
   }
 }
