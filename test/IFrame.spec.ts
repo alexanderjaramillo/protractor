3e171774767fe94bbf98eb6569087090b992e85c
Alexander Jaramillo <john.jara@gmail.com>
2017-10-28 09:31:44 -0500

    working-iframes (#21)

--- test/IFrame.spec.ts
+++ test/IFrame.spec.ts
@@ -1,15 +1,19 @@
 import { browser } from 'protractor';
 import { IFramePage } from '../src/page';
 
-fdescribe('Javascript Code Execution' , () => {
+fdescribe('Given a page with Iframes' , () => {
   const iframe: IFramePage = new IFramePage();
   
-  describe('Open Browser', () => {
+  describe('open browser', () => {
     beforeAll(async () => {
       await browser.get('http://toolsqa.com/iframe-practice-page/');  
     });
     
-    describe('Modify the Height of the IFrame', () => {
+    it('should get the title', async () => {
+      expect(await iframe.getTitle()).toBe('Sample Iframe page');  
+    });
+
+    describe('modify the height', () => {
       beforeAll(async () => {
         await iframe.modifyIframeHeight(600);
       });
@@ -17,6 +21,26 @@ fdescribe('Javascript Code Execution' , () => {
       it('should get the new height', async () => {
         expect(await iframe.getIframeHeight()).toBe(600);  
       });
+
+      describe('switch to Iframe 1', () => {
+        beforeAll(async () => {
+          await iframe.switchIframe1();  
+        });
+        
+        it('should get the title of the Iframe', async () => {
+          expect(await iframe.getTitle()).toBe('Practice Automation Form');  
+        });
+  
+        describe('switch to main context', () => {
+          beforeAll(async () => {
+            await iframe.switchMain();  
+          });
+          
+          it('should get the title of the main context again', async () => {
+            expect(await iframe.getTitle()).toBe('Sample Iframe page');  
+          });
+        }); 
+      });  
     });
   });
 });
